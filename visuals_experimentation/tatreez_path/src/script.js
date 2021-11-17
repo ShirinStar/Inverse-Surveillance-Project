befeef06f5d5bb2c86d9d4a0d675364986ba3a14
shirinstar <shirinanlen@gmail.com>
2021-11-17 14:33:34 -0500

    tatreez path test

---
+++ visuals_experimentation/tatreez_path/src/script.js
@@ -0,0 +1,180 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader'
+
+let SVGMesh;
+let geometry;
+let material;
+let positions;
+let opacity;
+let lines = [];
+
+const gui = new dat.GUI()
+
+const canvas = document.querySelector('.webgl')
+
+const scene = new THREE.Scene()
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 10, 5000)
+camera.position.z = 500
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+///svg Tatreez///
+
+const loader = new SVGLoader();
+const SVGViewBox = 512;
+// load a SVG resource
+loader.load(
+  // resource URL
+  'tatreez512.svg',
+  // called when the resource is loaded
+  function (data) {
+
+    const paths = data.paths;
+
+    let points = []
+
+    let maxPoints = paths.length * 100;
+    positions = new Float32Array(maxPoints * 3)
+    opacity = new Float32Array(maxPoints)
+
+    let randX = (Math.random() - 0.5) * 10
+    let randY = (Math.random() - 0.5) * 10
+
+    paths.forEach(line => {
+      line.subPaths.forEach(currentPoint => {
+        points.push(
+          new THREE.Vector3(
+            currentPoint.currentPoint.x - SVGViewBox / 2 + randX,
+            currentPoint.currentPoint.y - SVGViewBox / 2 + randY,
+            0
+          )
+        )
+      })
+    })
+
+    lines.push({
+      path: data.paths,
+      length: paths.length,
+      points: points,
+      currentPos: 0,
+      speed: 1
+    })
+
+    for (let i = 0; i < maxPoints; i++) {
+      positions.set([Math.random(), Math.random(), 0], i * 3)
+      opacity.set([Math.random() / 5], i)
+    }
+
+    geometry = new THREE.BufferGeometry()
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute('opacity', new THREE.BufferAttribute(opacity, 1));
+
+
+    material = new THREE.ShaderMaterial({
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+      transparent: true,
+      // depthTest: true,
+      depthWrite: false,
+      // alphaTest: 0.001,
+      blending: THREE.AdditiveBlending
+    })
+
+
+    SVGMesh = new THREE.Points(geometry, material)
+
+    scene.add(SVGMesh)
+  },
+
+  // called when loading is in progresses
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+
+  function (error) {
+    console.log('An error happened');
+
+  }
+);
+
+// ///
+
+
+
+
+
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+const clock = new THREE.Clock()
+
+const updateTrails = () => {
+
+  let j = 0;
+
+  lines.forEach(line => {
+
+    line.currentPos += line.speed
+    line.currentPos = line.currentPos % line.points.length
+
+    for (let i = 0; i < 100; i++) {
+      let index = (line.currentPos + i) % line.points.length
+      let showPoint = line.points[index]
+      positions.set([showPoint.x, showPoint.y, showPoint.z], j * 3)
+      opacity.set([i / 300], j)
+      j++
+    }
+  })
+
+  if (SVGMesh) {
+    SVGMesh.geometry.attributes.position.array = positions
+    SVGMesh.geometry.attributes.position.needsUpdate = true;
+  }
+}
+
+const render = () => {
+  renderer.render(scene, camera);
+}
+
+const animate = () => {
+  controls.update()
+
+  updateTrails()
+
+  requestAnimationFrame(animate);
+
+  render();
+}
+
+animate()
---
+++ visuals_experimentation/tatreez_path/src/shaders/fragment.glsl
@@ -0,0 +1,28 @@
+varying vec2 vUv;
+varying float vOpacity;
+
+void main()
+{
+  vec2 uv = vec2(gl_PointCoord.x, 1.0 - gl_PointCoord.y);
+  vec2 centralUv = 2.0 * uv - 1.0;
+
+  vec3 originalColor = vec3(4.0/255.0, 10.0/255.0, 20.0/255.0);
+  vec4 color = vec4(0.08 / length(centralUv)); //length( centralUV)= between 0- square of 2
+  color.rgb = min(vec3(10.0), color.rgb);
+
+  color.rgb *= originalColor * 120.0;
+
+  color *= vOpacity;
+
+  color.a = min(1.0, color.a)* 10.0;
+
+  float disc = length(centralUv);
+
+
+  //gl_FragColor = vec4(1.0 - disc, 0.0, 0.0, 1.0 )* vOpacity;
+  gl_FragColor = vec4(color.rgb, color.a);
+  
+  if(gl_FragColor.r<0.1 && gl_FragColor.b<0.1 && gl_FragColor.g<0.1) discard;
+
+
+}
